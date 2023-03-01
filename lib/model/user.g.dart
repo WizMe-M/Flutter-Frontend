b6f13f9ba7c329b859d79b5087048e98b4a0e034
WizMe-M <timkin.moxim@mail.ru>
2023-03-01 12:24:17 +0300

    generated files for user

---
+++ lib/model/user.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_User _$$_UserFromJson(Map<String, dynamic> json) => _$_User(
+      username: json['username'] as String,
+      password: json['password'] as String,
+      email: json['email'] as String,
+    );
+
+Map<String, dynamic> _$$_UserToJson(_$_User instance) => <String, dynamic>{
+      'username': instance.username,
+      'password': instance.password,
+      'email': instance.email,
+    };

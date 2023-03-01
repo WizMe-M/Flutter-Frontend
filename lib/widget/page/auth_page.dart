2a021f038e0b5e61fc74452cf726d7758bb46583
WizMe-M <timkin.moxim@mail.ru>
2023-03-01 18:39:59 +0300

    sign in button handler

--- lib/widget/page/auth_page.dart
+++ lib/widget/page/auth_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_frontend/dio_client.dart';
 import 'package:flutter_frontend/widget/utils/dynamic_auth_widget.dart';
 
 class AuthPage extends StatefulWidget {
@@ -9,10 +10,51 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
+  final DioClient _client = DioClient();
+
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
   bool isPasswordHidden = true;
+
+  void onSignInPressed(String username, String password) async {
+    var response =
+        await _client.authenticate(username: username, password: password);
+    if (response == null) {
+      var alert = const AlertDialog(
+        title: Text('Auth:'),
+        content: Text('Authorization failed. Try to restart API'),
+      );
+      showDialog(
+        context: context,
+        builder: (context) => alert,
+      );
+      return;
+    }
+
+    var alert = AlertDialog(
+        title: const Text('Auth Attempt: '),
+        content: Text(response.message!),
+        actions: [
+          TextButton(
+              onPressed: () {
+                if (response.error!.isNotEmpty) {
+                  Navigator.of(context, rootNavigator: true).pop();
+                } else {
+                  Navigator.of(context, rootNavigator: true).pop();
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return const Text('Authorized');
+                      },
+                    ),
+                  );
+                }
+              },
+              child: const Text('OK'))
+        ]);
+    showDialog(context: context, builder: (context) => alert);
+  }
 
   void togglePasswordObscure() {
     setState(() => isPasswordHidden = !isPasswordHidden);
@@ -34,7 +76,7 @@ class _AuthPageState extends State<AuthPage> {
                 DynamicInputWidget(
                   controller: _usernameController,
                   prefIcon: Icons.person,
-                  labelText: 'Enter Username',
+                  labelText: 'Username',
                   textInputAction: TextInputAction.next,
                 ),
                 const SizedBox(
@@ -43,7 +85,7 @@ class _AuthPageState extends State<AuthPage> {
                 DynamicInputWidget(
                   controller: _passwordController,
                   prefIcon: Icons.password,
-                  labelText: 'Enter Password',
+                  labelText: 'Password',
                   textInputAction: TextInputAction.done,
                   obscureText: isPasswordHidden,
                   isPasswordField: true,
@@ -52,7 +94,12 @@ class _AuthPageState extends State<AuthPage> {
                 const SizedBox(
                   height: 20,
                 ),
-                ElevatedButton(onPressed: () {}, child: const Text('Sign In')),
+                ElevatedButton(
+                    onPressed: () {
+                      onSignInPressed(
+                          _usernameController.text, _passwordController.text);
+                    },
+                    child: const Text('Sign In')),
                 const Spacer(),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,

f37b50bf54595391d6b77796961dc7c6abbd6b2e
WizMe-M <timkin.moxim@mail.ru>
2023-03-01 18:33:26 +0300

    dio client with auth method

---
+++ lib/dio_client.dart
@@ -0,0 +1,42 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_frontend/model/model_response.dart';
+import 'package:flutter_frontend/model/user.dart';
+
+class DioClient {
+  final Dio _dio = Dio(BaseOptions(
+      connectTimeout: const Duration(milliseconds: 3500),
+      receiveTimeout: const Duration(minutes: 10),
+      sendTimeout: const Duration(milliseconds: 3500)));
+
+  static const String authHeader = 'Authorization';
+
+  final _protocol = "http";
+  final _ip = "localhost";
+  final _port = 5781;
+
+  String get _baseUrl => '$_protocol://$_ip:$_port';
+
+  Future<ModelResponse?> authenticate(
+      {required String username, required String password}) async {
+    Response? response;
+    var inputted = User(userName: username, password: password);
+
+    try {
+      response = await _dio.post('$_baseUrl/token',
+          data: inputted.toJson(),
+          options: Options(receiveDataWhenStatusError: true));
+    } on DioError catch (e) {
+      return e.response?.data != null
+          ? ModelResponse.fromJson(e.response!.data)
+          : null;
+    }
+
+    var modelResponse = ModelResponse.fromJson(response.data);
+    var user = User.fromJson(modelResponse.data);
+
+    // 'Authorization' Header with Bearer JWT-token for next requests in this session
+    _dio.options.headers[authHeader] = 'Bearer ${user.accessToken}';
+
+    return modelResponse;
+  }
+}

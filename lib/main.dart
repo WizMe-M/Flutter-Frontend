972c813c7cf7836a1bd1433c8cb0a1b08c222e05
WizMe-M <timkin.moxim@mail.ru>
2023-03-01 19:06:38 +0300

    set min/max size for window

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_frontend/widget/page/auth_page.dart';
+import 'package:window_size/window_size.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('Flutter Frontend');
+    setWindowMinSize(const Size(300, 400));
+    setWindowMaxSize(const Size(1600, 900));
+  }
+
   runApp(const MyApp());
 }
 

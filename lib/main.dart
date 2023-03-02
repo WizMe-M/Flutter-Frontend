0ff68b7b8b2bde8e8adf25c190371dfed18ddd27
WizMe-M <timkin.moxim@mail.ru>
2023-03-02 16:55:08 +0300

    routing in app

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_frontend/widget/page/auth_page.dart';
 import 'package:flutter_frontend/widget/page/registration_page.dart';
+import 'package:flutter_frontend/widget/routing/app_route.dart';
+import 'package:flutter_frontend/widget/routing/enum_app_page.dart';
+import 'package:go_router/go_router.dart';
 import 'package:window_size/window_size.dart';
 
 void main() {
@@ -14,21 +17,35 @@ void main() {
     setWindowMaxSize(const Size(1600, 900));
   }
 
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final GoRouter _router = GoRouter(
+    initialLocation: AppPage.signIn.path,
+    routes: [
+      AppRoute(
+        page: AppPage.signIn,
+        builder: (context, state) => const AuthPage(),
+      ),
+      AppRoute(
+        page: AppPage.signUp,
+        builder: (context, state) => const RegistrationPage(),
+      )
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
+      routerConfig: _router,
       debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const RegistrationPage(),
     );
   }
 }

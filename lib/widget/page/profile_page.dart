8c3005b31d4dc7a926153fcd2fc089febe038a01
WizMe-M <timkin.moxim@mail.ru>
2023-03-02 22:11:50 +0300

    add profile page

--- lib/widget/page/profile_page.dart
+++ lib/widget/page/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_frontend/model/user.dart';
+import 'package:flutter_frontend/widget/utils/dynamic_auth_widget.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key, required this.authorized}) : super(key: key);
@@ -11,11 +12,45 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  late TextEditingController _emailController;
+  late TextEditingController _usernameController;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController(text: widget.authorized.email);
+    _usernameController =
+        TextEditingController(text: widget.authorized.userName);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Text('User Email: ${widget.authorized.email}'),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Center(
+              child: SizedBox(
+            height: 400,
+            width: 300,
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text('Your profile data:', style: TextStyle(fontSize: 24)),
+              const SizedBox(height: 40),
+              DynamicInputWidget(
+                  controller: _emailController,
+                  readonly: true,
+                  labelText: 'Email',
+                  prefixIcon: Icons.email),
+              const SizedBox(height: 20),
+              DynamicInputWidget(
+                  controller: _usernameController,
+                  readonly: true,
+                  labelText: 'Username',
+                  prefixIcon: Icons.account_box_sharp),
+            ]),
+          )),
+        ),
       ),
     );
   }
